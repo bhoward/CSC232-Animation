f0cedf959ccf1c926e74217fa5f5e829a7ed5591
Brian Howard <bhoward@depauw.edu>
2023-04-20 17:29:00 -0400

    Trying to generalize Easing

--- csc232/Easing.java
+++ csc232/Easing.java
@@ -10,88 +10,78 @@
 
 package csc232;
 
+import java.util.function.Function;
+
 /**
  * @author bhoward
- *
  */
-public class Easing
+public interface Easing
 {
-   private static final double C1 = 1.70158;
-   private static final double C2 = C1 * 1.525;
-
-   private static double square(double x)
+   default double in(double x)
    {
-      return x * x;
+      return 1 - out(1 - x);
    }
 
-   private static double cube(double x)
+   default double out(double x)
    {
-      return x * x * x;
+      return 1 - in(1 - x);
    }
 
-   public static double linear(double x)
+   default double inOut(double x)
    {
-      return x;
+      return (x < 0.5) ? in(2 * x) / 2 : 1 - in(2 - 2 * x) / 2;
    }
 
-   public static double quadIn(double x)
+   static Easing fromIn(Function<Double, Double> f)
    {
-      return square(x);
+      return new Easing()
+      {
+         @Override
+         public double in(double x)
+         {
+            return f.apply(x);
+         }
+      };
    }
 
-   public static double quadOut(double x)
+   static Easing fromOut(Function<Double, Double> f)
    {
-      return 1 - square(1 - x);
+      return new Easing()
+      {
+         @Override
+         public double out(double x)
+         {
+            return f.apply(x);
+         }
+      };
    }
 
-   public static double quadInOut(double x)
-   {
-      return (x < 0.5) ? 2 * square(x) : 1 - 2 * square(1 - x);
-   }
-
-   public static double cubicIn(double x)
-   {
-      return cube(x);
-   }
-
-   public static double cubicOut(double x)
-   {
-      return 1 - cube(1 - x);
-   }
-
-   public static double cubicInOut(double x)
-   {
-      return (x < 0.5) ? 4 * cube(x) : 1 - 4 * cube(1 - x);
-   }
-
-   public static double sineIn(double x)
-   {
-      return 1 - Math.cos(x * Math.PI / 2);
-   }
-
-   public static double sineOut(double x)
-   {
-      return Math.sin(x * Math.PI / 2);
-   }
-
-   public static double sineInOut(double x)
-   {
-      return (1 - Math.cos(Math.PI * x)) / 2;
-   }
-
-   public static double backIn(double x)
-   {
-      return (C1 + 1) * cube(x) - C1 * square(x);
-   }
-
-   public static double backOut(double x)
-   {
-      return 1 - (C1 + 1) * cube(1 - x) + C1 * square(1 - x);
-   }
-
-   public static double backInOut(double x)
-   {
-      return (x < 0.5) ? 4 * (C2 + 1) * cube(x) - 2 * C2 * square(x)
-               : 1 - 4 * (C2 + 1) * cube(1 - x) + 2 * C2 * square(1 - x);
-   }
+   static final Easing LINEAR = fromIn(x -> x);
+   
+   static final Easing QUAD = fromIn(x -> x * x);
+   
+   static final Easing CUBIC = fromIn(x -> x * x * x);
+   
+   static final Easing SINE = fromOut(x -> Math.sin(x));
 }
+// public class Easing
+// {
+// private static final double C1 = 1.70158;
+// private static final double C2 = C1 * 1.525;
+//
+// public static double backIn(double x)
+// {
+// return (C1 + 1) * cube(x) - C1 * square(x);
+// }
+//
+// public static double backOut(double x)
+// {
+// return 1 - (C1 + 1) * cube(1 - x) + C1 * square(1 - x);
+// }
+//
+// public static double backInOut(double x)
+// {
+// return (x < 0.5) ? 4 * (C2 + 1) * cube(x) - 2 * C2 * square(x)
+// : 1 - 4 * (C2 + 1) * cube(1 - x) + 2 * C2 * square(1 - x);
+// }
+// }
